32b824ee46ba0f912c4b284ff62527b900fe518b
ChereGG <cheregitudor@yahoo.com>
2023-05-03 20:14:04 +0300

    Initial commit.

---
+++ src/main/java/com/example/ubb/QuizWebsiteBackend/QuizWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ubb.QuizWebsiteBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizWebsiteBackendApplication.class, args);
+	}
+
+}
